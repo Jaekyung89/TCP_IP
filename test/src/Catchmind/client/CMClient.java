14dd8c45f474632099a45e8d79ecc0c95a40ceec
Jaekyung89 <kjkstone08@naver.com>
2023-11-27 15:55:41 +0900

    굿

--- test/src/Catchmind/client/CMClient.java
+++ test/src/Catchmind/client/CMClient.java
@@ -1,8 +1,22 @@
 package Catchmind.client;
 
+import javax.swing.JFrame;
+
 public class CMClient {
 
-	public static void main(String[] args) {
+	public CMClient(String title) {
+		JFrame Client_Main = new JFrame();
+		Client_Main.setTitle(title);
+		Client_Main.setBounds(0, 0, 1000, 800);
+		Client_Main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Client_Main.getContentPane().setLayout(null);
+		Client_Main.setLocationRelativeTo(null);
+		Client_Main.setResizable(false);
+		Client_Main.setVisible(true);
 	}
-
+	
+	public static void main(String[] args) {
+		new CMClient("안녕");
+	}
+	
 }
